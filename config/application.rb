7b5231097d112acafdf3ef8b41f742b557943568
HirotoKawakami <hk10soccer@gmail.com>
2018-09-08 19:05:33 +0900

    edit apprication.rb

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Archives
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Asia/Tokyo'
+    config.assets.initialize_on_precompile = false
   end
 end
