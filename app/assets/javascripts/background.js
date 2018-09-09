245b8b1778de74872be2246ff55db42893928dab
HirotoKawakami <hk10soccer@gmail.com>
2018-09-09 15:32:56 +0900

    edit css and background.js

--- app/assets/javascripts/background.js
+++ app/assets/javascripts/background.js
@@ -1,57 +1,79 @@
-class Ball {
-    constructor(value) {
-      this.value = value;
-      this.x = random(0, width);
-      this.y = randomGaussian(height/2, height/3);
-      this.vx = 1;
-      this.vy = 0;
-      this.size = random(20, 50);
-      this.t = random(10000);
-      this.color = color(random(255), random(255), random(255), random(255));
-    }
-  
-    update() {
-      let t = this.t+0.02;
-  
-      let x1 = this.x;
-      let y1 = this.y;
-      let vx1 = this.vx;
-      let vy1 = this.vy;
-  
-      x1 += vx1;
-      x1 = width <= x1 ? 0 : x1;
-  
-      const fSpring = -2*(y1-mouseY)/height;
-      const dx = abs(mouseX-x1);
-      const fMouse = (mouseIsPressed && x1 < mouseX) ? - 10*pow(dx+Number.EPSILON, -0.5) : 0;
-      vy1 += fSpring + fMouse;
-      vy1 *= 0.98;
-      y1 += vy1 + 10*noise(t);
-  
-      this.x = x1;
-      this.y = y1;
-      this.vy = vy1;
-    }
-  }
-  
-  
-  let balls = [];
-  
-  function setup() {
-    createCanvas(windowWidth, 200);
-    noStroke();
-    for(let i=0; i<50; i++) {
-      let ball = new Ball(1);
-      balls.push(ball);
-    }
-  }
-  
-  function draw() {
-    background(255, 255, 255);
-    balls.forEach(ball => {
-      ball.update();
-      fill(ball.color);
-      let bSize = ball.size;
-      ellipse(ball.x, ball.y, bSize, bSize);
-    });
-  }
+var drops = [];
+var smallDropCount = 250;
+var largeDropCount = 50;
+var dropCount = smallDropCount + largeDropCount;
+var snowMode = true;
+
+function Drop (c, z) {
+	this.color = c;
+	this.z = z;
+	this.x = random(width);
+	this.y = random(-windowHeight, 0) * this.z;
+	this.size = map(this.z, 1, 3, 5, 10);
+	this.ySpeed = map(this.z, 1, 3, 1.5, 3.5);
+	
+	this.xSpeed = random(0, map(this.z, 1, 3, 0.1, 0.2));
+	this.xOffset = 0;
+	this.maxXOffset = map(this.z, 1, 3, 1.5, 1);
+	
+	this.draw = function () {
+		if(snowMode){
+			fill(this.color);
+			noStroke();
+			ellipse(this.x, this.y, this.size);
+		}else {
+			stroke(this.color);
+			strokeWeight(this.z);
+			line(this.x, this.y, this.x, this.y + this.size);
+		}
+	}
+	
+	this.fall = function () {
+		this.y += this.ySpeed;
+		this.x += this.xSpeed;
+		this.xOffset += this.xSpeed;
+		if (this.y > height) {
+			this.y = random(-windowHeight, 0) * this.z;
+		}
+		
+		if( abs(this.xOffset) >= abs(this.maxXOffset) ){
+			this. xSpeed *= -1;
+		}
+	}
+}
+
+function setup () {
+	createCanvas(windowWidth, windowHeight);
+	textSize(30);
+	var colors = [ 
+		color(135, 206, 250),
+		color(240, 248, 255),
+ 		color(0, 191, 255),
+		color(30, 144, 255)
+	];
+	for(var i = 0; i <= smallDropCount; i++) {
+		drops.push(
+			new Drop(
+				colors[floor(random(0, colors.length))],
+				1
+			)
+		);
+	}
+	for(var i = 0; i <= largeDropCount; i++) {
+		drops.push(
+			new Drop(
+				colors[floor(random(0, colors.length))],
+				random( 2, 3 )
+			)
+		);
+	}
+}
+
+function draw () {
+	background(255, 255, 255);
+	for(var i = 0; i <= dropCount; i++) {
+		var d = drops[i];
+		d.fall();
+		d.draw();
+	}
+}
