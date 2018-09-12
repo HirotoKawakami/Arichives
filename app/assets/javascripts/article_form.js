4eceed1014c5932df1bf45503a333a13d96a90d7
HirotoKawakami <hk10soccer@gmail.com>
2018-09-13 08:47:20 +0900

    edit article_form.js

--- app/assets/javascripts/article_form.js
+++ app/assets/javascripts/article_form.js
@@ -10,13 +10,11 @@ $(function() {
         success: function(file, response) {
             console.log(file);
                 // file.previewElementでpreview要素のhtmlにアクセス
-                new ClipboardJS('.btn');
-                var html = `<div class="btn" data-clipboard-text=${response.url}>Copy</div>`;
 
                 $(file.previewElement).addClass('dz-success');
                 $(file.previewElement).find('.dz-remove').hide();
                 $(file.previewElement).find('.dz-progress').hide();
-                $(file.previewElement).find('.dz-filename').find('span').html(html);
+                $(file.previewElement).find('.dz-filename').find('span').text(response.url);
                 
             },
         
